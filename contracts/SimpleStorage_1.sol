14095738c8ffe1dcdc3fd616abd423980d6ca920
nadimpalli bhargav <bhargavraju98@gmail.com>
2024-03-01 08:40:21 +0000

    💡added smart contract file

---
+++ contracts/SimpleStorage_1.sol
@@ -0,0 +1,17 @@
+//Solidity Version
+pragma solidity 0.6.4;
+
+contract SimpleStorage {
+    string public storedData;
+
+    event myEventTest(string eventOutput);
+
+    function set(string memory myText) public {
+        storedData = myText;
+        emit myEventTest(myText);
+    }
+
+    function get() public view returns (string memory) {
+        return storedData;
+    }
+}
